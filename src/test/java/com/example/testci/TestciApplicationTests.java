3b57515133873288701927d56d48b6c4b775cf9a
JamalJavadov <camalcavadovwork@gmail.com>
2025-10-17 09:29:12 +0400

    first commit

---
+++ src/test/java/com/example/testci/TestciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
